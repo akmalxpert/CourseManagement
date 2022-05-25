9dc8b6f489919808e878115eb3bad0d56fc1ec6d
sah2020 <akmaljonsamandarov@gmail.com>
2022-05-25 14:09:20 +0500

    initial setup

---
+++ src/test/java/uz/exadel/coursemanagement/CourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.exadel.coursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
