26fdcaa5f310d2d3ea502cea23f0a24afea53e25
sah2020 <akmaljonsamandarov@gmail.com>
2022-05-26 08:47:30 +0500

    Course entity created

---
+++ src/main/java/uz/exadel/coursemanagement/entity/Course.java
@@ -0,0 +1,22 @@
+package uz.exadel.coursemanagement.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Course {
+    @Id
+    @GeneratedValue()
+    private UUID id;
+
+    private String name;
+
+    private String description;
+}
