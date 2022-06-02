8b57dfddb4658ed61cf04b21e84291954da1b760
sah2020 <akmaljonsamandarov@gmail.com>
2022-06-02 10:28:12 +0500

    spring run/properties updated

---
+++ controller/src/main/java/uz/exadel/ControllerApplication.java
@@ -0,0 +1,13 @@
+package uz.exadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class ControllerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+}
