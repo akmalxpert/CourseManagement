482b191e3471358bc79737e11330fcc202cfdfb5
sah2020 <akmaljonsamandarov@gmail.com>
2022-06-13 10:01:00 +0500

    exceptions wrrapped around ResponseItem

--- service/src/main/java/uz/exadel/exception/GlobalExceptionHandler.java
+++ service/src/main/java/uz/exadel/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import uz.exadel.dtos.ResponseItem;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -17,29 +18,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {MissingMandatoryFieldException.class})
     @ResponseStatus(BAD_REQUEST)
-    public HttpEntity<?> handleMissingMandatoryFieldException(MissingMandatoryFieldException ex) {
+    public ResponseEntity<ResponseItem> handleMissingMandatoryFieldException(MissingMandatoryFieldException ex) {
         ex.printStackTrace();
-        return ResponseEntity.status(BAD_REQUEST).body(ex.getMessage());
+        ResponseItem responseItem = new ResponseItem(null, ex.getMessage(), false);
+        return ResponseEntity.status(BAD_REQUEST).body(responseItem);
     }
 
     @ExceptionHandler(value = {ValidationException.class})
     @ResponseStatus(BAD_REQUEST)
     public HttpEntity<?> handleValidationException(ValidationException ex) {
         ex.printStackTrace();
-        return ResponseEntity.status(BAD_REQUEST).body(ex.getMessage());
+        ResponseItem responseItem = new ResponseItem(null, ex.getMessage(), false);
+        return ResponseEntity.status(BAD_REQUEST).body(responseItem);
     }
 
     @ExceptionHandler(value = {SchoolNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
     public HttpEntity<?> handleSchoolNotFoundException(SchoolNotFoundException ex) {
         ex.printStackTrace();
-        return ResponseEntity.status(NOT_FOUND).body("School not found");
+        ResponseItem responseItem = new ResponseItem(null, "School not found", false);
+        return ResponseEntity.status(NOT_FOUND).body(responseItem);
     }
 
     @ExceptionHandler(value = {Exception.class})
     public HttpEntity<?> handleException(Exception ex) {
         ex.printStackTrace();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong. Keep calm");
+        ResponseItem responseItem = new ResponseItem(null, "Something went wrong. Keep calm", false);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseItem);
     }
 
 }
