cf670d7429f9e5219464bc98d55f7d9bfe7ea28c
sah2020 <akmaljonsamandarov@gmail.com>
2022-08-24 10:06:50 +0500

    tomcat config fixed

--- web/src/main/java/uz/exadel/ControllerApplication.java
+++ web/src/main/java/uz/exadel/ControllerApplication.java
@@ -2,11 +2,18 @@ package uz.exadel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class ControllerApplication {
+public class ControllerApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ControllerApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ControllerApplication.class, args);
     }
