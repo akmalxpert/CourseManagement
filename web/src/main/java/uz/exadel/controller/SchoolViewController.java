3b2c0b1234c39c7eb4c0da7becb93efe4da8f00c
sah2020 <akmaljonsamandarov@gmail.com>
2022-08-24 21:25:31 +0500

    logging library updated to log4j2

--- web/src/main/java/uz/exadel/controller/SchoolViewController.java
+++ web/src/main/java/uz/exadel/controller/SchoolViewController.java
@@ -1,23 +1,23 @@
 package uz.exadel.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Slf4j
 @RestController
 @RequestMapping("/view/school")
-@RequiredArgsConstructor
 @CrossOrigin
 public class SchoolViewController {
 
+    private static final Logger logger = LogManager.getLogger(SchoolViewController.class);
+
     @GetMapping(value = {"/", "/home", "/index", "default"})
     public ModelAndView showHomePage() {
-        log.info(this.getClass().getSimpleName() + ":=======>Showing Home Page.");
+        logger.info(this.getClass().getSimpleName() + ":=======>Showing Home Page.");
         return new ModelAndView("index"); // Here index is a jsp page name
     }
 
