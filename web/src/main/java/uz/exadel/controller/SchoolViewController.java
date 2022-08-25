54222f830715925bb5a8e8869218f8cb95a53807
sah2020 <akmaljonsamandarov@gmail.com>
2022-08-25 14:51:08 +0500

    all schools view page completed (thymeleaf)

--- web/src/main/java/uz/exadel/controller/SchoolViewController.java
+++ web/src/main/java/uz/exadel/controller/SchoolViewController.java
@@ -2,23 +2,33 @@ package uz.exadel.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import uz.exadel.entity.School;
+import uz.exadel.service.SchoolService;
 
-@RestController
-@RequestMapping("/view/school")
-@CrossOrigin
+import java.util.List;
+
+@Controller
+@RequestMapping("/school")
 public class SchoolViewController {
 
+    private final SchoolService schoolService;
     private static final Logger logger = LogManager.getLogger(SchoolViewController.class);
 
-    @GetMapping(value = {"/", "/home", "/index", "default"})
-    public ModelAndView showHomePage() {
-        logger.info(this.getClass().getSimpleName() + ":=======>Showing Home Page.");
-        return new ModelAndView("index"); // Here index is a jsp page name
+    public SchoolViewController(SchoolService schoolService) {
+        this.schoolService = schoolService;
+    }
+
+    @GetMapping()
+    public String showAll(Model model) {
+        Object list = schoolService.getAll().getData();
+
+        model.addAttribute("schoolList", (List<School>) list);
+        logger.info(this.getClass().getSimpleName() + ":=======>Showing Schools Main Page.");
+        return "school";
     }
 
 }
