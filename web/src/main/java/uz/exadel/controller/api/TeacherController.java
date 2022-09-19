374b5ca2d305f9e13bd9cf24bcecaeef1d1ba508
sah2020 <akmaljonsamandarov@gmail.com>
2022-09-19 16:29:26 +0500

    completed api controller for teacher

---
+++ web/src/main/java/uz/exadel/controller/api/TeacherController.java
@@ -0,0 +1,51 @@
+package uz.exadel.controller.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.exadel.dtos.ResponseData;
+import uz.exadel.dtos.TeacherDTO;
+import uz.exadel.service.TeacherService;
+import uz.exadel.validations.TeacherValidationService;
+
+@RestController
+@RequestMapping("/api/teacher")
+@CrossOrigin
+public class TeacherController {
+    private final TeacherValidationService teacherValidationService;
+    private final TeacherService teacherService;
+
+    public TeacherController(TeacherValidationService teacherValidationService, TeacherService teacherService) {
+        this.teacherValidationService = teacherValidationService;
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseData> add(@RequestBody TeacherDTO teacherDTO) {
+        teacherValidationService.validateCreate(teacherDTO);
+        return ResponseEntity.ok(teacherService.add(teacherDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseData> getById(@PathVariable String id) {
+        teacherValidationService.validateGetById(id);
+        return ResponseEntity.ok(teacherService.get(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseData> getBySchoolId(@RequestParam String schoolId) {
+        teacherValidationService.validateGetBySchoolId(schoolId);
+        return ResponseEntity.ok(teacherService.getBySchoolId(schoolId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseData> delete(@PathVariable String id) {
+        teacherValidationService.validateDelete(id);
+        return ResponseEntity.ok(teacherService.delete(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseData> update(@PathVariable String id, @RequestBody TeacherDTO teacherDTO) {
+        teacherValidationService.validateUpdate(teacherDTO, id);
+        return ResponseEntity.ok(teacherService.update(teacherDTO, id));
+    }
+}
