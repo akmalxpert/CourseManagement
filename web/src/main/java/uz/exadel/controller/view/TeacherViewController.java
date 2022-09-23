9d64ce7e62cc8e988fb141cdd08a35a33fc31bab
sah2020 <akmaljonsamandarov@gmail.com>
2022-09-23 11:27:01 +0500

    completed view controller for teacher

---
+++ web/src/main/java/uz/exadel/controller/view/TeacherViewController.java
@@ -0,0 +1,65 @@
+package uz.exadel.controller.view;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import uz.exadel.controller.api.CourseController;
+import uz.exadel.controller.api.SchoolController;
+import uz.exadel.controller.api.TeacherController;
+import uz.exadel.dtos.ResponseData;
+import uz.exadel.dtos.TeacherDTO;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/teacher")
+public class TeacherViewController {
+    private final TeacherController teacherController;
+    private final SchoolController schoolController;
+    private final CourseController courseController;
+
+
+    public TeacherViewController(TeacherController teacherController, SchoolController schoolController, CourseController courseController) {
+        this.teacherController = teacherController;
+        this.schoolController = schoolController;
+        this.courseController = courseController;
+    }
+
+    @GetMapping
+    public String showAll(@RequestParam("schoolId") String schoolId, Model model) {
+        ResponseEntity<ResponseData> response = teacherController.getBySchoolId(schoolId);
+        Object list = Objects.requireNonNull(response.getBody()).getData();
+
+        model.addAttribute("teacherList", list);
+        model.addAttribute("teacherDTO", new TeacherDTO());
+
+        ResponseEntity<ResponseData> schoolsResponse = schoolController.getSchools();
+        Object schoolList = Objects.requireNonNull(schoolsResponse.getBody()).getData();
+        model.addAttribute("schoolList", schoolList);
+
+        ResponseEntity<ResponseData> coursesResponse = courseController.getCoursesBySchoolId(schoolId);
+        Object courseList = Objects.requireNonNull(coursesResponse.getBody()).getData();
+        model.addAttribute("courseList", courseList);
+
+        return "teacher";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("teacherDTO") TeacherDTO teacherDTO) {
+        teacherController.add(teacherDTO);
+        return "redirect:/teacher?schoolId=" + teacherDTO.getSchoolId();
+    }
+
+    @PutMapping("/update/{id}")
+    public String update(@PathVariable(value = "id") String id, @ModelAttribute("teacherDTO") TeacherDTO teacherDTO) {
+        teacherController.update(id, teacherDTO);
+        return "redirect:/teacher?schoolId=" + teacherDTO.getSchoolId();
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") String id, Model model) {
+        teacherController.delete(id);
+        return "redirect:/school";
+    }
+}
