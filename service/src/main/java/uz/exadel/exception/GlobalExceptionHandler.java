bdf6d90f5e8c1b10382bdda213320a5d48ab4992
sah2020 <akmaljonsamandarov@gmail.com>
2025-08-30 07:16:52 +0500

    Fix global exception handler

--- service/src/main/java/uz/exadel/exception/GlobalExceptionHandler.java
+++ service/src/main/java/uz/exadel/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,7 +18,7 @@ import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = {MissingMandatoryFieldException.class})
@@ -27,111 +26,111 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ResponseData> handleMissingMandatoryFieldException(
             MissingMandatoryFieldException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Validation error [{}]: {} - Request: {}", 
+        logger.warn("Validation error [{}]: {} - Request: {}",
                 errorId, ex.getMessage(), getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, ex.getMessage(), BAD_REQUEST.value());
         return ResponseEntity.status(BAD_REQUEST).body(responseData);
     }
 
     @ExceptionHandler(value = {ValidationException.class})
     @ResponseStatus(BAD_REQUEST)
-    public HttpEntity<?> handleValidationException(ValidationException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleValidationException(ValidationException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Validation error [{}]: {} - Request: {}", 
+        logger.warn("Validation error [{}]: {} - Request: {}",
                 errorId, ex.getMessage(), getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, ex.getMessage(), BAD_REQUEST.value());
         return ResponseEntity.status(BAD_REQUEST).body(responseData);
     }
 
     @ExceptionHandler(value = {SchoolNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    public HttpEntity<?> handleSchoolNotFoundException(SchoolNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleSchoolNotFoundException(SchoolNotFoundException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Entity not found [{}]: School not found - Request: {}", 
+        logger.warn("Entity not found [{}]: School not found - Request: {}",
                 errorId, getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "School not found", NOT_FOUND.value());
         return ResponseEntity.status(NOT_FOUND).body(responseData);
     }
 
     @ExceptionHandler(value = {GroupNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    public HttpEntity<?> handleGroupNotFoundException(GroupNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleGroupNotFoundException(GroupNotFoundException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Entity not found [{}]: Group not found - Request: {}", 
+        logger.warn("Entity not found [{}]: Group not found - Request: {}",
                 errorId, getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "Group not found", NOT_FOUND.value());
         return ResponseEntity.status(NOT_FOUND).body(responseData);
     }
 
     @ExceptionHandler(value = {CourseNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    public HttpEntity<?> handleCourseNotFoundException(CourseNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleCourseNotFoundException(CourseNotFoundException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Entity not found [{}]: Course not found - Request: {}", 
+        logger.warn("Entity not found [{}]: Course not found - Request: {}",
                 errorId, getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "Course not found", NOT_FOUND.value());
         return ResponseEntity.status(NOT_FOUND).body(responseData);
     }
 
     @ExceptionHandler(value = {TeacherNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    public HttpEntity<?> handleTeacherNotFoundException(TeacherNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleTeacherNotFoundException(TeacherNotFoundException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Entity not found [{}]: Teacher not found - Request: {}", 
+        logger.warn("Entity not found [{}]: Teacher not found - Request: {}",
                 errorId, getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "Teacher not found", NOT_FOUND.value());
         return ResponseEntity.status(NOT_FOUND).body(responseData);
     }
 
     @ExceptionHandler(value = {StudentNotFoundException.class})
     @ResponseStatus(NOT_FOUND)
-    public HttpEntity<?> handleStudentNotFoundException(StudentNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleStudentNotFoundException(StudentNotFoundException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Entity not found [{}]: Student not found - Request: {}", 
+        logger.warn("Entity not found [{}]: Student not found - Request: {}",
                 errorId, getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "Student not found", NOT_FOUND.value());
         return ResponseEntity.status(NOT_FOUND).body(responseData);
     }
 
     @ExceptionHandler(value = {DataIntegrityViolationException.class})
     @ResponseStatus(BAD_REQUEST)
-    public HttpEntity<?> handleConstraintViolationException(DataIntegrityViolationException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleConstraintViolationException(DataIntegrityViolationException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Data integrity violation [{}]: {} - Request: {}", 
+        logger.warn("Data integrity violation [{}]: {} - Request: {}",
                 errorId, ex.getMessage(), getRequestInfo(request));
-        
-        ResponseData responseData = new ResponseData(null, 
-                "This operation affects some other entities. Please delete all related entities to avoid this error.", 
+
+        ResponseData responseData = new ResponseData(null,
+                "This operation affects some other entities. Please delete all related entities to avoid this error.",
                 BAD_REQUEST.value());
         return ResponseEntity.status(BAD_REQUEST).body(responseData);
     }
 
     @ExceptionHandler(value = {EmptyResultDataAccessException.class})
     @ResponseStatus(BAD_REQUEST)
-    public HttpEntity<?> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.warn("Empty result data access [{}]: {} - Request: {}", 
+        logger.warn("Empty result data access [{}]: {} - Request: {}",
                 errorId, ex.getMessage(), getRequestInfo(request));
-        
+
         ResponseData responseData = new ResponseData(null, "The requested resource was not found.", BAD_REQUEST.value());
         return ResponseEntity.status(BAD_REQUEST).body(responseData);
     }
 
     @ExceptionHandler(value = {Exception.class})
-    public HttpEntity<?> handleException(Exception ex, WebRequest request) {
+    public ResponseEntity<ResponseData> handleException(Exception ex, WebRequest request) {
         String errorId = generateErrorId();
-        logger.error("Unexpected error [{}]: {} - Request: {} - Stack trace: ", 
+        logger.error("Unexpected error [{}]: {} - Request: {} - Stack trace: ",
                 errorId, ex.getMessage(), getRequestInfo(request), ex);
-        
-        ResponseData responseData = new ResponseData(null, 
-                "An unexpected error occurred. Please contact support with error ID: " + errorId, 
+
+        ResponseData responseData = new ResponseData(null,
+                "An unexpected error occurred. Please contact support with error ID: " + errorId,
                 INTERNAL_SERVER_ERROR.value());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseData);
     }
